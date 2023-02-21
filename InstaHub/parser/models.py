1d231becdf3a8308126f59fc96398827be5c3862
Alex <zarjrw1994@gmail.com>
2023-02-22 02:06:04 +0300

    Added a model for storing authorization data in instagram accounts of the service.

--- InstaHub/parser/models.py
+++ InstaHub/parser/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class InstaHub_instagram_accounts(models.Model):
+    """Модель хранит данные для авторизации в instagram аккаунтов принадлежавших сервису."""
+
+    login_inst = models.CharField(max_length=40, unique=True, blank=False, verbose_name='Instagram логин')
+    password_inst = models.CharField(max_length=40, blank=False, verbose_name='Instagram пароль')
+    email = models.EmailField(max_length=40, unique=True, blank=True,  verbose_name= 'email')
+    email_password = models.CharField(max_length=40, blank=True, verbose_name='Пароль email')
+    date_joined = models.DateTimeField(default=timezone.now, verbose_name='Дата регистрации')
+    date_update = models.DateTimeField(auto_now=True, verbose_name='Дата обновления')
+    blocked = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'{self.pk}. {self.login_inst} -blocked: {self.blocked}'
